8c2a0cb35e8c864429d2cb39898ba484ba60b83b
liangkun <lk706010@163.com>
2017-03-10 19:01:48 +0800

    commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -34,7 +34,46 @@ Page({
   data: {   
     // initial data
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    imgUrls: [
+      // 'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      // 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      '../../images/lb1.jpg','../../images/lb2.jpg','../../images/lb3.jpg',
+    ],
+    indicatorDors: true,
+    autoPlay: true,
+    markers: [{
+      //iconPath: "/resources/others.png",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color:"#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      //iconPath: '/resources/location.png',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -52,5 +91,15 @@ Page({
         userInfo:userInfo
       })
     })
+  },
+
+regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
   }
 })
